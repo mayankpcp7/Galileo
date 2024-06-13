e80c8fcff1cd675b10e429702a5577d25f5017cf
mayankpcp7 <jariwalmayank123@gmail.com>
2024-06-13 16:40:40 +0530

    merge code

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,40 +1,63 @@
-"use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { SubHeadingBg } from "./common/Icons";
 import Navbar from "./common/Navbar";
-import { HeaderDiscord , HeaderLinked, HeaderTelegram } from "./common/Icons";
+import { HeaderDiscord, HeaderLinked, HeaderTelegram } from "./common/Icons";
+
+const videoSources = [
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+];
+
 const Header = () => {
-    const [toggle, setToggle] = React.useState(false);
-    const scrollToSection = (elementId) => {
-      setToggle(true);
-      const element = document.getElementById(elementId);
-      element?.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest",
-      });
-    };
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setFadeIn(true);
+      setTimeout(() => {
+        setCurrentVideoIndex((prevIndex) =>
+          prevIndex === videoSources.length - 1 ? 0 : prevIndex + 1
+        );
+        setFadeIn(false);
+      }, 1000);
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const scrollToSection = (elementId) => {
+    
+  };
+
   return (
-    <div className="min-h-screen  flex flex-col relative overflow-hidden">
+    <div className="min-h-screen flex flex-col relative overflow-hidden">
       <div className="relative z-[100]">
         <Navbar />
       </div>
-      <video
-        autoPlay
-        loop
-        muted
-        className="absolute top-0 left-0 w-full h-full object-cover"
-      >
-        <source
-          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
-      <div className="absolute flex z-[100] items-center  right-[-21%] md:right-0 hover:right-0 transition-all duration-300 ease-linear top-[50%] translate-y-[-50%]">
+      <div className="absolute top-0 left-0 w-full h-full object-cover">
+        {videoSources.map((source, index) => (
+          <video
+            key={index}
+            autoPlay
+            loop
+            muted
+            className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 ${
+              currentVideoIndex === index ? "opacity-100" : "opacity-0"
+            }`}
+            style={{ zIndex: currentVideoIndex === index ? 1 : -1 }}
+          >
+            <source src={source} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ))}
+      </div>
+      <div className="absolute flex z-[100] items-center right-[-21%] md:right-0 hover:right-0 transition-all duration-300 ease-linear top-[50%] translate-y-[-50%]">
         <div className="bg-white w-[15px] h-[145px] rounded-l-full"></div>
-        <div className="backdrop-blur bg-white-opacity-[17%]  px-[20px] py-[70px]">
+        <div className="backdrop-blur bg-white-opacity-[17%] px-[20px] py-[70px]">
           <Image
             src="/assets/images/svg/header/twitter.svg"
             width={36}
@@ -53,8 +76,7 @@ const Header = () => {
           </div>
         </div>
       </div>
-
-      <div className="flex-grow relative z-10  flex items-center justify-center">
+      <div className="flex-grow relative z-10 flex items-center justify-center">
         <div className="max-w-[1164px] mx-auto px-3">
           <div className="flex justify-center items-center flex-col">
             <div className="flex items-center relative justify-center">
@@ -74,11 +96,15 @@ const Header = () => {
             </p>
           </div>
         </div>
-          </div>
-          <div  className='absolute z-20 -translate-x-1/2 bottom-10 left-1/2'>
-
-      <button onClick={() => scrollToSection('footer')} className={`flex w-6 sm:w-11 h-[72px] sm:h-[76px] rounded-full border border-pink relative after:absolute sm:after:w-[27px] after:w-[15px] after:h-[27px] after:rounded-full after:bg-pink after:duration-300 after:left-1/2 after:-translate-x-1/2 ${toggle ? "after:top-9" : "after:top-2"}`}></button>
-             </div>
+      </div>
+      <div className="absolute z-20 -translate-x-1/2 bottom-10 left-1/2">
+        <button
+          onClick={() => scrollToSection("footer")}
+          className={`flex w-6 sm:w-11 h-[72px] sm:h-[76px] rounded-full border border-pink relative after:absolute sm:after:w-[27px] after:w-[15px] after:h-[27px] after:rounded-full after:bg-pink after:duration-300 after:left-1/2 after:-translate-x-1/2 ${
+            fadeIn ? "after:top-9" : "after:top-2"
+          }`}
+        ></button>
+      </div>
     </div>
   );
 };
